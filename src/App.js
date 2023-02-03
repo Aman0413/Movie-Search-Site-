aeae06763ed92bdd6b57145c1f6d25652c5cda12
Aman Verma <aman@Amans-MacBook-Air.local>
2023-02-03 21:30:25 +0530

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import "./App.css";
-
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Home />
     </div>
   );
